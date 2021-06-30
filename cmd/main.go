269f4b9a5f857bb60fc81ce3e6dc1dbb3c8944f2
Henelik <kelyn@crandall.me>
2021-06-29 19:01:45 -0500

    chore: vendor

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,4 @@
 package main
 
-import qtrade "github.com/Henelik/qtrade-api-go/qtrade/v1"
-
-func main () {
-	config := qtrade.Configuration{
-		Auth: "1:11111111111111111111",
-	}
-
-	client := qtrade.NewQtradeClient(config)
-
-	req := client.Client.R().
+func main() {
 }
--- go.mod
+++ go.mod
@@ -4,7 +4,8 @@ go 1.16
 
 require (
 	bou.ke/monkey v1.0.2
-	github.com/go-resty/resty/v2 v2.6.0
+	github.com/go-resty/resty/v2 v2.6.0 // indirect
+	github.com/jarcoal/httpmock v1.0.8
 	github.com/stretchr/testify v1.7.0
 	gopkg.in/resty.v1 v1.12.0 // indirect
 )
