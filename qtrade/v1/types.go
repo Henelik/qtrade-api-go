2d94600fe399c0477917ab986fe218a651129094
Henelik <kelyn@crandall.me>
2021-07-06 13:20:59 -0500

    feat: add user info endpoint

--- qtrade/v1/types.go
+++ qtrade/v1/types.go
@@ -1,18 +1,48 @@
 package v1
 
+import "time"
+
 // base types
 
+type UserInfo struct {
+	CanLogin       bool           `json:"can_login"`
+	CanTrade       bool           `json:"can_trade"`
+	CanWithdraw    bool           `json:"can_withdraw"`
+	Email          string         `json:"email"`
+	EmailAddresses []EmailAddress `json:"email_addresses"`
+	FirstName      string         `json:"fname"`
+	LastName       string         `json:"lname"`
+	ID             int            `json:"id"`
+	ReferralCode   string         `json:"referral_code"`
+	TFAEnabled     bool           `json:"tfa_enabled"`
+	Verification   string         `json:"verification"`
+	VerifiedEmail  bool           `json:"verified_email"`
+	WithdrawLimit  int            `json:"withdraw_limit"`
+}
+
+type EmailAddress struct {
+	Address   string    `json:"address"`
+	CreatedAt time.Time `json:"created_at"`
+	ID        int       `json:"id"`
+	IsPrimary bool      `json:"is_primary"`
+	Verified  bool      `json:"verified"`
+}
+
 type Balance struct {
-	Currency string
-	Balance  string
+	Currency string `json:"currency"`
+	Balance  string `json:"balance"`
 }
 
 // API results
 
-type GetBalancesResult struct {
-	Data Balances
+type GetUserInfoResult struct {
+	Data struct {
+		User UserInfo `json:"user"`
+	} `json:"data"`
 }
 
-type Balances struct {
-	Balances []Balance
+type GetBalancesResult struct {
+	Data struct {
+		Balances []Balance `json:"balances"`
+	} `json:"data"`
 }
