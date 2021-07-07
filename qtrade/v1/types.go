afa9077f71fb79e1ade50ff3ee101fc728c5db0c
Henelik <kelyn@crandall.me>
2021-07-06 23:51:47 -0500

    feat: add user markets endpoint

--- qtrade/v1/types.go
+++ qtrade/v1/types.go
@@ -33,6 +33,29 @@ type Balance struct {
 	Balance  string `json:"balance"`
 }
 
+type Order struct {
+	BaseAmount            float64   `json:"base_amount,string"`
+	CreatedAt             time.Time `json:"created_at"`
+	ID                    int       `json:"id"`
+	MarketAmount          float64   `json:"market_amount,string"`
+	MarketAmountRemaining float64   `json:"market_amount_remaining,string"`
+	MarketID              int       `json:"market_id"`
+	Open                  bool      `json:"open"`
+	OrderType             string    `json:"order_type"`
+	Price                 float64   `json:"price,string"`
+	Trades                []Trade   `json:"trades"`
+}
+
+type Trade struct {
+	BaseAmount   float64   `json:"base_amount,string"`
+	BaseFee      float64   `json:"base_fee,string"`
+	CreatedAt    time.Time `json:"created_at"`
+	ID           int       `json:"id"`
+	MarketAmount float64   `json:"market_amount,string"`
+	Price        float64   `json:"price,string"`
+	Taker        bool      `json:"taker"`
+}
+
 // API results
 
 type GetUserInfoResult struct {
@@ -45,4 +68,15 @@ type GetBalancesResult struct {
 	Data struct {
 		Balances []Balance `json:"balances"`
 	} `json:"data"`
+}
+
+type GetUserMarketResult struct {
+	Data UserMarketData `json:"data"`
+}
+
+type UserMarketData struct {
+	BaseBalance   float64 `json:"base_balance,string"`
+	ClosedOrders  []Order `json:"closed_orders"`
+	MarketBalance float64 `json:"market_balance,string"`
+	OpenOrders    []Order `json:"open_orders"`
 }
