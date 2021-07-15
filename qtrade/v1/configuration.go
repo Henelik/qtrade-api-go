418c4683a955e26907fbfa21b61131a2cd434dfa
Henelik <kelyn@crandall.me>
2021-07-15 17:38:00 -0500

    chore: add mapstructure tags to config

--- qtrade/v1/configuration.go
+++ qtrade/v1/configuration.go
@@ -5,7 +5,7 @@ import (
 )
 
 type Configuration struct {
-	HMACKeypair string
-	Endpoint    string
-	Timeout     time.Duration
+	HMACKeypair string        `mapstructure:"hmac_keypair"`
+	Endpoint    string        `mapstructure:"endpoint"`
+	Timeout     time.Duration `mapstructure:"timeout"`
 }
