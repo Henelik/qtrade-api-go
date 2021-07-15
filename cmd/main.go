68ccc812c96b4f599c161ee8ab539e29db034695
Henelik <kelyn@crandall.me>
2021-07-15 17:37:36 -0500

    feat: create a test script to get balances

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,44 @@
 package main
 
+import (
+	"context"
+	"fmt"
+
+	"github.com/spf13/viper"
+
+	v1 "github.com/Henelik/qtrade-api-go/qtrade/v1"
+)
+
 func main() {
+	config := new(v1.Configuration)
+
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	err = viper.Unmarshal(config)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("config: %#v\n", config)
+
+	client, err := v1.NewQtradeClient(*config)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("auth: %#v\n", client.Auth)
+
+	balances, err := client.GetBalances(context.Background(), nil)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("balances: %#v\n", balances)
 }
