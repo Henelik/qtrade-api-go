33162aa6c21761200cb743a8a9f18085a5fae2f6
Henelik <kelyn@crandall.me>
2021-10-20 23:23:32 -0500

    chore: remove cmd and scripts folders

--- scripts/currencies.py
+++
@@ -1,8 +0,0 @@
-from qtrade_client.api import QtradeAPI, APIException
-from pprint import pprint
-
-if __name__ == "__main__":
-    api = QtradeAPI("https://api.qtrade.io", key="")
-
-    for code, currency in api.currencies.items():
-        print(code + ": " + str(currency["precision"]))
